a581721a86000b682a54debff8f9adc8623fd5f4
nhann <nhanhnv@gmail.com>
2024-04-10 17:51:47 +0700

    clean code

--- src/doline.cpp
+++ src/doline.cpp
@@ -10,7 +10,7 @@ const byte RIGHT_MOTOR_EN = 11;
 // Định nghĩa các chân kết nối với cảm biến
 const int sensorPins[] = {A0, A1, A2, A3, A4};
 const int MAX_SPEED = 255;
-const int MIN_SPEED = 65;
+const int MIN_SPEED = 50;
 const int DEFAULT_SPEED = 105;
 
 int caclulatePID(int error);
@@ -20,7 +20,6 @@ void moveForward(int speed);
 void turnRight(int speed);
 void turnLeft(int speed);
 void solve(int pid);
-void turnLeft90(int speed);
 
 float p = 0;
 float i = 0;
@@ -53,27 +52,19 @@ void setup()
 
 void loop()
 {
-
     // in giá trị cảm biến
     readSensors();
-    // for (int i = 0; i < numSensors; i++) {
-    //     Serial.print(sensorValues[i]);
-    //     Serial.println(" ");
-    // }
     int error = calculeError();
     Serial.print("Error: ");
     Serial.println(error);
     int pid = caclulatePID(error);
     Serial.print("PID: ");
     Serial.println(pid);
-
     solve(pid);
     delay(50);
     // delay(2000);
 }
-
 // duyệt các giá trị của cảm biến viết hàm đưa các giá trị >1000 về 1 và <1000 về 0
-
 int readSensors()
 {
     for (int i = 0; i < numSensors; i++)
@@ -97,7 +88,7 @@ int calculeError()
 
 int caclulatePID(int error)
 {
-    float kp = 3;
+    float kp = 3.5;
     float ki = 0.01;
     float kd = 0.05;
 
@@ -110,28 +101,15 @@ int caclulatePID(int error)
 
 void solve(int pid)
 {
-    int speed = abs(pid) * 12;
+    int speed = abs(pid) * 5;
     speed = (speed == 0) ? DEFAULT_SPEED : speed;
     speed = (speed < MIN_SPEED) ? MIN_SPEED : speed;
-
-    if (speed == 255) {
-        delay(300);
-        turnLeft90(speed);
-        return;
-    }
-
     if (pid > 0)
-    {
-        turnLeft(abs(speed));
-    }
+        turnLeft(speed);
     else if (pid < 0)
-    {
-        turnRight(abs(speed));
-    }
+        turnRight(speed);
     else
-    {
-        moveForward(abs(speed));
-    }
+        moveForward(speed);
 }
 
 // Hàm điều khiển động cơ với tốc độ có thể điều chỉnh
@@ -151,7 +129,7 @@ void turnRight(int speed)
 {
     Serial.println("Turn right: ");
     Serial.println(speed);
-    analogWrite(LEFT_MOTOR_EN, speed+7.65);
+    analogWrite(LEFT_MOTOR_EN, speed + 7.65);
     analogWrite(RIGHT_MOTOR_EN, speed / 2);
     digitalWrite(LEFT_MOTOR_PIN1, HIGH);
     digitalWrite(LEFT_MOTOR_PIN2, LOW);
@@ -164,24 +142,12 @@ void turnLeft(int speed)
 {
     Serial.println("Turn left: ");
     Serial.println(speed);
-    analogWrite(LEFT_MOTOR_EN, speed / 2+7.65);
+    analogWrite(LEFT_MOTOR_EN, speed / 2 + 7.65);
     analogWrite(RIGHT_MOTOR_EN, speed);
     digitalWrite(LEFT_MOTOR_PIN1, LOW);
     digitalWrite(LEFT_MOTOR_PIN2, HIGH);
     digitalWrite(RIGHT_MOTOR_PIN1, HIGH);
     digitalWrite(RIGHT_MOTOR_PIN2, LOW);
-}
-
-void turnLeft90(int speed)
-{
-    Serial.println("Turn left: ");
-    Serial.println(speed);
-    analogWrite(LEFT_MOTOR_EN, 160);
-    analogWrite(RIGHT_MOTOR_EN, 100);
-    digitalWrite(LEFT_MOTOR_PIN1, HIGH);
-    digitalWrite(LEFT_MOTOR_PIN2, LOW);
-    digitalWrite(RIGHT_MOTOR_PIN1, LOW);
-    digitalWrite(RIGHT_MOTOR_PIN2, HIGH);
 }
 
 // Hàm đi thẳng
